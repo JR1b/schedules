2ddffdc430d8a1d652dd28a164c6cc9cd0bc49c6
JoaoRibeiro <jpr9993@gmail.com>
2023-07-13 11:40:48 +0100

    chore: added storybook

---
+++ libs/frontend/schedules/src/lib/Schedules.stories.tsx
@@ -0,0 +1,12 @@
+import type { Meta } from '@storybook/react';
+import { Schedules } from './Schedules';
+
+const Story: Meta<typeof Schedules> = {
+  component: Schedules,
+  title: 'Schedules',
+};
+export default Story;
+
+export const Primary = {
+  args: {},
+};
