6a97e995463623c33c631cae187e208233301ddc
JoaoRibeiro <jpr9993@gmail.com>
2023-07-14 11:38:16 +0100

    refactor: update api types and old modules

---
+++ apps/api/src/types/schedule.types.ts
@@ -0,0 +1,13 @@
+export type Schedule = {
+  id: number;
+  name: string;
+  description: string;
+  isRetired: boolean;
+  tasksCount: number;
+  startPoint: string;
+  endPoint: string;
+  dayOfWeek: number;
+  dayOfMonth: number;
+  startDate: string;
+  endDate: string;
+};
